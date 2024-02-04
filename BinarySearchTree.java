3efd02213ec4679de287f36c58c65450f8832bd9
Amartya Roy Chowdhury <amartya.officials@gmail.com>
2024-02-04 10:27:05 -0500

    added the treenode defination

--- BinarySearchTree.java
+++ BinarySearchTree.java
@@ -1,6 +1,32 @@
+import com.sun.source.tree.Tree;
+
 public class BinarySearchTree {
 
      public class TreeNode{
+          TreeNode left;
+          TreeNode right;
+          int value;
+
+          public TreeNode insert(TreeNode n , int value){
+               if(n==null) {
+                    return createNode(value);
+               }
+
+               if(value < n.value){
+                    n.left = insert(n.left,value);
+               }else if( value > n.value){
+                    n.right = insert(n.right,value);
+               }
+               return n;
+          }
+
+          public TreeNode createNode(int value){
+               TreeNode treeNode = new TreeNode();
+               treeNode.value = value;
+               treeNode.right = null;
+               treeNode.left = null;
+               return treeNode;
+          }
 
      }
 }
