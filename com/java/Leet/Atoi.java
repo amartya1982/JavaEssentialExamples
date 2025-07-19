643e84ded7390daa198487417bcddbb4e75bff61
amartya1982 <amartya.roychowdhury@gmail.com>
2025-07-19 01:47:21 -0400

    added the treenode defination

--- com/java/Leet/Atoi.java
+++ com/java/Leet/Atoi.java
@@ -1,2 +1,26 @@
-package com.java.Leet;public class Atoi {
+package com.java.Leet;
+
+public class Atoi {
+    public static void main(String args[]){
+        System.out.println(sol("32"));
+    }
+
+    static int sol(String s){
+        StringBuffer result = new StringBuffer();
+        char[] charArray = s.toCharArray();
+        for(int i=0; i<charArray.length; i++){
+           if(charArray[i] != ' ' && result.isEmpty()){
+                if(charArray[i] == '-'){
+                    result.append(charArray[i]);
+                }else if (!Character.isDigit(charArray[i])){
+                    break;
+                }
+           }else if (Character.isDigit(charArray[i])){
+               result.append(charArray[i]);
+           }else if(!Character.isDigit(charArray[i])){
+               break;
+           }
+        }
+        return Integer.parseInt(result.toString());
+    }
 }
